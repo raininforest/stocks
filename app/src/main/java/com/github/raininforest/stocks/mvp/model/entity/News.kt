24bb14d30eed684cfe902f052eb0eebffc35bcc1
Sergey Velesko <s.velesko@yandex.ru>
2021-08-02 22:13:35 +0300

    Made main entities

--- app/src/main/java/com/github/raininforest/stocks/mvp/model/entity/News.kt
+++ app/src/main/java/com/github/raininforest/stocks/mvp/model/entity/News.kt
@@ -4,6 +4,7 @@ package com.github.raininforest.stocks.mvp.model.entity
  * Created by Sergey Velesko on 01.08.2021
  */
 data class News(
-    val id: String
-    //TODO
+    val header: String,
+    val summary: String,
+    val sourceUrl: String
 )
