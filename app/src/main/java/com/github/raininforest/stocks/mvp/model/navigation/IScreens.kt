790e1a71854d852fc5f266f12e4e104fbe9e87c3
Sergey Velesko <s.velesko@yandex.ru>
2021-08-02 22:47:03 +0300

    Created cicerone interface

--- app/src/main/java/com/github/raininforest/stocks/mvp/model/navigation/IScreens.kt
+++ app/src/main/java/com/github/raininforest/stocks/mvp/model/navigation/IScreens.kt
@@ -1,8 +1,11 @@
 package com.github.raininforest.stocks.mvp.model.navigation
 
+import com.github.terrakok.cicerone.Screen
+
 /**
  * Created by Sergey Velesko on 01.08.2021
  */
 interface IScreens {
-    //TODO
+    fun stocks(): Screen
+    fun news(ticker: String): Screen
 }
