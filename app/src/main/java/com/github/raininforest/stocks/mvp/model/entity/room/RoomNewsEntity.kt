b263fd1178137e1bbe50e21efe805042220421f8
Sergey Velesko <s.velesko@yandex.ru>
2021-08-02 22:20:16 +0300

    Made room entities

--- app/src/main/java/com/github/raininforest/stocks/mvp/model/entity/room/RoomNewsEntity.kt
+++ app/src/main/java/com/github/raininforest/stocks/mvp/model/entity/room/RoomNewsEntity.kt
@@ -1,9 +1,24 @@
 package com.github.raininforest.stocks.mvp.model.entity.room
 
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
 /**
  * Created by Sergey Velesko on 01.08.2021
  */
+@Entity(
+    foreignKeys = [ForeignKey(
+        entity = RoomStockEntity::class,
+        parentColumns = ["ticker"],
+        childColumns = ["tickerId"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
 data class RoomNewsEntity(
-    val id:String
-    //TODO
+    @PrimaryKey val id: String,
+    val header: String,
+    val summary: String,
+    val sourceUrl: String,
+    val tickerId: String
 )
--- app/src/main/java/com/github/raininforest/stocks/mvp/model/entity/room/RoomStockEntity.kt
+++ app/src/main/java/com/github/raininforest/stocks/mvp/model/entity/room/RoomStockEntity.kt
@@ -1,9 +1,17 @@
 package com.github.raininforest.stocks.mvp.model.entity.room
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 /**
  * Created by Sergey Velesko on 01.08.2021
  */
+@Entity
 data class RoomStockEntity(
-    val id:String
-    //TODO
+    @PrimaryKey
+    val ticker: String,
+    val companyName: String,
+    val price: String,
+    val change: String,
+    val logoUrl: String
 )
