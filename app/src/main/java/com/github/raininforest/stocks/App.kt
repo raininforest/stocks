62034437e54905b17c1da511cf2e29a8fe09cd37
Sergey Velesko <s.velesko@yandex.ru>
2021-08-07 13:59:47 +0300

    Commented dagger stuff

--- app/src/main/java/com/github/raininforest/stocks/App.kt
+++ app/src/main/java/com/github/raininforest/stocks/App.kt
@@ -1,8 +1,6 @@
 package com.github.raininforest.stocks
 
 import android.app.Application
-import com.github.raininforest.stocks.di.AppComponent
-import com.github.raininforest.stocks.di.DaggerAppComponent
 
 /**
  * Application class
@@ -11,15 +9,15 @@ import com.github.raininforest.stocks.di.DaggerAppComponent
  */
 class App : Application() {
 
-    lateinit var appComponent: AppComponent
+//    lateinit var appComponent: AppComponent
 
     override fun onCreate() {
         super.onCreate()
         instance = this
 
-        appComponent = DaggerAppComponent
-            .builder()
-            .build()
+//        appComponent = DaggerAppComponent
+//            .builder()
+//            .build()
     }
 
     companion object {
