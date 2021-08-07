bdf02808ca52e55606832088835fc73839d307bc
Sergey Velesko <s.velesko@yandex.ru>
2021-08-07 14:22:38 +0300

    Implement caches

--- app/src/main/java/com/github/raininforest/stocks/mvp/model/cache/room/RoomNewsCache.kt
+++ app/src/main/java/com/github/raininforest/stocks/mvp/model/cache/room/RoomNewsCache.kt
@@ -1,8 +1,30 @@
 package com.github.raininforest.stocks.mvp.model.cache.room
 
+import com.github.raininforest.stocks.mvp.model.cache.INewsCache
+import com.github.raininforest.stocks.mvp.model.entity.News
+import com.github.raininforest.stocks.mvp.model.entity.room.db.Database
+import com.github.raininforest.stocks.mvp.model.toNewsList
+import com.github.raininforest.stocks.mvp.model.toRoomNewsEntityList
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.lang.RuntimeException
+
 /**
  * Created by Sergey Velesko on 01.08.2021
  */
-class RoomNewsCache {
-    //TODO
+class RoomNewsCache(val db: Database) : INewsCache {
+    override fun getNews(ticker: String): Single<List<News>> =
+        Single.fromCallable {
+            val roomStockEntity = db.stocksDao.findTicker(ticker)
+                ?: throw RuntimeException("No ticker in cache: $ticker")
+            return@fromCallable db.newsDao.findByTicker(roomStockEntity.ticker).toNewsList()
+        }.subscribeOn(Schedulers.io())
+
+    override fun putNews(ticker: String, news: List<News>): Completable =
+        Completable.fromAction {
+            val roomStockEntity = db.stocksDao.findTicker(ticker)
+                ?: throw RuntimeException("No ticker in cache: $ticker")
+            db.newsDao.insert(news.toRoomNewsEntityList(ticker))
+        }.subscribeOn(Schedulers.io())
 }
--- app/src/main/java/com/github/raininforest/stocks/mvp/model/cache/room/RoomStocksCache.kt
+++ app/src/main/java/com/github/raininforest/stocks/mvp/model/cache/room/RoomStocksCache.kt
@@ -1,8 +1,26 @@
 package com.github.raininforest.stocks.mvp.model.cache.room
 
+import com.github.raininforest.stocks.mvp.model.cache.IStocksCache
+import com.github.raininforest.stocks.mvp.model.entity.Stock
+import com.github.raininforest.stocks.mvp.model.entity.room.db.Database
+import com.github.raininforest.stocks.mvp.model.toRoomStockEntityList
+import com.github.raininforest.stocks.mvp.model.toStockList
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+
 /**
  * Created by Sergey Velesko on 01.08.2021
  */
-class RoomStocksCache {
-    //TODO
+class RoomStocksCache(val db: Database) : IStocksCache {
+    override fun getStocks(): Single<List<Stock>> =
+        Single.fromCallable {
+            db.stocksDao.getAll().toStockList()
+        }.subscribeOn(Schedulers.io())
+
+
+    override fun putStocks(stocks: List<Stock>): Completable =
+        Completable.fromAction {
+            db.stocksDao.insert(stocks.toRoomStockEntityList())
+        }.subscribeOn(Schedulers.io())
 }
