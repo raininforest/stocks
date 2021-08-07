ab403f8f8c458f21257127ec8e8ede74a08b31eb
Sergey Velesko <s.velesko@yandex.ru>
2021-08-07 21:31:33 +0300

    Implement NetworkStatus

--- app/src/main/java/com/github/raininforest/stocks/ui/network/NetworkStatus.kt
+++ app/src/main/java/com/github/raininforest/stocks/ui/network/NetworkStatus.kt
@@ -1,8 +1,38 @@
 package com.github.raininforest.stocks.ui.network
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkRequest
+import com.github.raininforest.stocks.mvp.model.network.INetworkStatus
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.subjects.BehaviorSubject
+
 /**
  * Created by Sergey Velesko on 31.07.2021
  */
-class NetworkStatus {
-    //TODO
+class NetworkStatus(context: Context): INetworkStatus {
+    private val statusSubject: BehaviorSubject<Boolean> = BehaviorSubject.create()
+
+    init {
+        statusSubject.onNext(false)
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val request = NetworkRequest.Builder().build()
+        connectivityManager.registerNetworkCallback(request, object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                statusSubject.onNext(true)
+            }
+
+            override fun onUnavailable() {
+                statusSubject.onNext(false)
+            }
+
+            override fun onLost(network: Network) {
+                statusSubject.onNext(false)
+            }
+        })
+    }
+
+    override fun isOnline() = statusSubject
+    override fun isOnlineSingle(): Single<Boolean> = statusSubject.first(false)
 }
