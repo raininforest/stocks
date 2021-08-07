8c49c1cbc0c9135063c0a926488f9167937f8416
Sergey Velesko <s.velesko@yandex.ru>
2021-08-07 21:25:34 +0300

    Provide client for logging, change Gson

--- app/src/main/java/com/github/raininforest/stocks/di/module/ApiModule.kt
+++ app/src/main/java/com/github/raininforest/stocks/di/module/ApiModule.kt
@@ -1,11 +1,15 @@
 package com.github.raininforest.stocks.di.module
 
+import com.github.raininforest.stocks.App
 import com.github.raininforest.stocks.mvp.model.api.IRemoteApi
-import com.google.gson.FieldNamingPolicy
+import com.github.raininforest.stocks.mvp.model.network.INetworkStatus
+import com.github.raininforest.stocks.ui.network.NetworkStatus
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -24,17 +28,30 @@ class ApiModule {
 
     @Singleton
     @Provides
-    fun gson() = GsonBuilder()
-        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-        .excludeFieldsWithoutExposeAnnotation()
+    fun provideClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+
+    @Singleton
+    @Provides
+    fun gson(): Gson = GsonBuilder()
         .create()
 
     @Singleton
     @Provides
-    fun api(@Named("baseUrl") baseUrl: String, gson: Gson): IRemoteApi = Retrofit.Builder()
-        .baseUrl(baseUrl)
-        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-        .addConverterFactory(GsonConverterFactory.create(gson))
-        .build()
-        .create(IRemoteApi::class.java)
+    fun api(@Named("baseUrl") baseUrl: String, gson: Gson, client: OkHttpClient): IRemoteApi =
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(client)
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(IRemoteApi::class.java)
+
+    @Singleton
+    @Provides
+    fun networkStatus(app: App): INetworkStatus = NetworkStatus(app)
 }
