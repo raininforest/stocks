fa69eb972bd56d2952ab153590829103568341b2
Sergey Velesko <s.velesko@yandex.ru>
2021-08-07 21:28:51 +0300

    Implement Screens

--- app/src/main/java/com/github/raininforest/stocks/ui/navigation/Screens.kt
+++ app/src/main/java/com/github/raininforest/stocks/ui/navigation/Screens.kt
@@ -1,17 +1,16 @@
 package com.github.raininforest.stocks.ui.navigation
 
 import com.github.raininforest.stocks.mvp.model.navigation.IScreens
-import com.github.terrakok.cicerone.Screen
+import com.github.raininforest.stocks.ui.fragment.NewsFragment
+import com.github.raininforest.stocks.ui.fragment.StocksFragment
+import com.github.terrakok.cicerone.androidx.FragmentScreen
 
 /**
  * Created by Sergey Velesko on 31.07.2021
  */
 class Screens : IScreens {
-    override fun stocks(): Screen {
-        TODO("Not yet implemented")
-    }
 
-    override fun news(ticker: String): Screen {
-        TODO("Not yet implemented")
-    }
+    override fun stocks() = FragmentScreen { StocksFragment.newInstance() }
+
+    override fun news(ticker: String) = FragmentScreen { NewsFragment.newInstance(ticker) }
 }
