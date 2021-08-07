0f78696cc578413745c72c5fb4d58f6f65ee5df6
Sergey Velesko <s.velesko@yandex.ru>
2021-08-07 14:05:10 +0300

    Implement Repositories

--- app/src/main/java/com/github/raininforest/stocks/mvp/model/repo/NewsRepository.kt
+++ app/src/main/java/com/github/raininforest/stocks/mvp/model/repo/NewsRepository.kt
@@ -1,14 +1,33 @@
 package com.github.raininforest.stocks.mvp.model.repo
 
+import com.github.raininforest.stocks.mvp.model.DateGenerator
+import com.github.raininforest.stocks.mvp.model.api.IRemoteApi
+import com.github.raininforest.stocks.mvp.model.cache.INewsCache
 import com.github.raininforest.stocks.mvp.model.entity.News
+import com.github.raininforest.stocks.mvp.model.network.INetworkStatus
+import com.github.raininforest.stocks.mvp.model.toNewsList
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 /**
  * Created by Sergey Velesko on 01.08.2021
  */
-class NewsRepository : INewsRepository {
-    override fun getNews(ticker: String): Single<List<News>> {
-        //TODO
-        return Single.just(emptyList())
-    }
+class NewsRepository(
+    val api: IRemoteApi,
+    val networkStatus: INetworkStatus,
+    val cache: INewsCache,
+    val dateGenerator: DateGenerator
+) : INewsRepository {
+    override fun getNews(ticker: String): Single<List<News>> =
+        networkStatus.isOnlineSingle().flatMap { isOnline ->
+            if (isOnline) {
+                api.news(ticker, dateGenerator.weekAgo(), dateGenerator.today())
+                    .flatMap { newsDtoList ->
+                        cache.putNews(ticker, newsDtoList.toNewsList())
+                            .toSingleDefault(newsDtoList.toNewsList())
+                    }
+            } else {
+                cache.getNews(ticker)
+            }
+        }.subscribeOn(Schedulers.io())
 }
--- app/src/main/java/com/github/raininforest/stocks/mvp/model/repo/StocksRepository.kt
+++ app/src/main/java/com/github/raininforest/stocks/mvp/model/repo/StocksRepository.kt
@@ -1,14 +1,59 @@
 package com.github.raininforest.stocks.mvp.model.repo
 
+import com.github.raininforest.stocks.mvp.model.api.IRemoteApi
+import com.github.raininforest.stocks.mvp.model.api.dto.StockDTO
+import com.github.raininforest.stocks.mvp.model.cache.IStocksCache
 import com.github.raininforest.stocks.mvp.model.entity.Stock
+import com.github.raininforest.stocks.mvp.model.network.INetworkStatus
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 /**
  * Created by Sergey Velesko on 01.08.2021
  */
-class StocksRepository : IStocksRepository {
-    override fun getStocks(): Single<List<Stock>> {
-        //TODO
-        return Single.just(emptyList())
-    }
+class StocksRepository(
+    val api: IRemoteApi,
+    val networkStatus: INetworkStatus,
+    val cache: IStocksCache
+) : IStocksRepository {
+
+    override fun getStocks(): Single<List<Stock>> =
+        networkStatus.isOnlineSingle().flatMap { isOnline ->
+            if (isOnline) {
+                api.stockList()
+                    .flatMap { requestStockInfoByTickerList(it) }
+            } else {
+                cache.getStocks()
+            }
+        }.subscribeOn(Schedulers.io())
+
+    private fun requestStockInfoByTickerList(listStockDTO: List<StockDTO>): Single<List<Stock>> =
+        Single.create { emitter ->
+            val resultList = mutableListOf<Stock>()
+            listStockDTO.subList(0, 10)
+                .forEach { stockDTO ->
+                    requestEachStockInfo(stockDTO)
+                        .subscribeOn(Schedulers.io())
+                        .subscribe(
+                            { resultList.add(it) },
+                            { it.printStackTrace() }
+                        )
+                }
+            emitter.onSuccess(resultList)
+        }
+
+    private fun requestEachStockInfo(stockDTO: StockDTO): Single<Stock> =
+        Single.zip(
+            api.companyProfile(stockDTO.symbol),
+            api.quote(stockDTO.symbol),
+            { p1, p2 ->
+                Stock(
+                    ticker = p1.ticker,
+                    companyName = p1.name,
+                    price = "${p2.c.toString()} ${p1.currency}",
+                    change = p2.dp.toString(),
+                    logoUrl = p1.logo
+                )
+            }
+        )
 }
